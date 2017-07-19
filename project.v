c638df6f724503a9b675d9d40d2b7c98541d589e
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2017-07-19 17:26:16 -0400

    now the game can be played with two input buttons

--- project.v
+++ project.v
@@ -16,12 +16,13 @@ module project(CLOCK_50, LEDR, KEY, HEX0, HEX1, HEX2, HEX3, HEX5, SW, LEDG, GPIO
   output [9:0] LEDR;
   input [0:0] SW;
 
-  input [0:0] GPIO;
+  input [1:0] GPIO;
   output [0:0] LEDG;
 
-  wire push_key;
-  assign push_key = GPIO[0];
-
+  wire push_key_1;
+  assign push_key_1 = GPIO[0];
+  wire push_key_2;
+  assign push_key_2 = GPIO[1];
   assign LEDG[0] = GPIO[0];
 
   wire [9:0] led;
@@ -88,12 +89,13 @@ module project(CLOCK_50, LEDR, KEY, HEX0, HEX1, HEX2, HEX3, HEX5, SW, LEDG, GPIO
     .hex_display(HEX3[6:0]),
     .signals(score[7:4]));
 
-  hex_accuracy teaching_assistant(
+  hex_accuracy grader(
     .hex(HEX5),
     .accuracy(accuracy));
 
   // just for test, have no meaning at all
-  localparam RHYTHM_MAP=191'b0000000100010000000001000001000000100101100000000000000001100001000000100000000000010000000100000001000000000000000000000001000100001000010000000001100000000000010100000000010000000001000001;
+  localparam RHYTHM_MAP_KEY_1 = 191'b0000000100010000000001000001000000100101100000000000000001100001000000100000000000010000000100000001000000000000000000000001000100001000010000000001100000000000010100000000010000000001000001;
+  localparam RHYTHM_MAP_KEY_2 = 191'b0000010001000001000000000010000010010011000000000000100000000000010000000000100000001000000000000100000000000000010000000010000000000010000001000100000000000001000000100000001000000100000000;
 
   clock_8hz apple_watch (
     .clk(CLOCK_50),
@@ -110,9 +112,11 @@ module project(CLOCK_50, LEDR, KEY, HEX0, HEX1, HEX2, HEX3, HEX5, SW, LEDG, GPIO
     .clk_50m(CLOCK_50),
     .load(KEY[0]),
     .rst(SW[0]),
-    .button(push_key),
+    .button_1(push_key_1),
+    .button_2(push_key_2),
     .is_start(if_shift),
-    .init_rhythm_map(RHYTHM_MAP),
+    .init_rhythm_map_key_1(RHYTHM_MAP_KEY_1),
+    .init_rhythm_map_key_2(RHYTHM_MAP_KEY_2),
     .rhythm_shifter_out(led[9:0]),
     .combo(combo[7:0]),
     .score(score[7:0]),
@@ -153,20 +157,21 @@ module control(clk, start, rst, enable_shift);
   end
 endmodule
 
-module datapath(clk_8, clk_50m, load, button, is_start, rst, init_rhythm_map, rhythm_shifter_out, combo, score, accuracy, x, y, colour);
-  input clk_8, clk_50m, is_start, rst, load, button;
-  input [190:0] init_rhythm_map;
-  reg [190:0] rhythm_shifter;
-  output wire [9:0] rhythm_shifter_out;
+module datapath(clk_8, clk_50m, load, button_1, button_2, is_start, rst, init_rhythm_map_key_1, init_rhythm_map_key_2, rhythm_shifter_out_key_1, rhythm_shifter_out_key_2, combo, score, accuracy, x, y, colour);
+  input clk_8, clk_50m, is_start, rst, load, button_1, button_2;
+  input [190:0] init_rhythm_map_key_1, init_rhythm_map_key_2;
+  reg [190:0] rhythm_shifter, rhythm_shifter_key_2;
+  output wire [9:0] rhythm_shifter_out_key_1, rhythm_shifter_out_key_2;
   output reg [7:0] combo = 8'b0;
   output reg [7:0] score = 8'b0;
   output reg [1:0] accuracy = 2'b0;
-  reg button_last_state = 1'b1;
+  reg button_1_last_state, button_2_last_state = 1'b1;
   output reg [7:0] x = 8'b0;
   output reg [6:0] y = 7'b0;
   output reg [2:0] colour = 3'b0;
 
-  assign rhythm_shifter_out[9:0] = rhythm_shifter[11:2];
+  assign rhythm_shifter_out_key_1[9:0] = rhythm_shifter[11:2];
+  assign rhythm_shifter_out_key_2[7:0] = rhythm_shifter_key_2[9:2]
 
   reg [14:0] position = 15'b0;
   reg [6:0] x_pos = 7'b0;
@@ -176,20 +181,23 @@ module datapath(clk_8, clk_50m, load, button, is_start, rst, init_rhythm_map, rh
     if (!rst) begin
       // reset
       rhythm_shifter <= 191'd0;
+      rhythm_shifter_key_2 <= 191'd0;
       score <= 8'b0;
       combo <= 8'b0;
     end else begin
       if (!load) begin
-        rhythm_shifter <= init_rhythm_map;
+        rhythm_shifter <= init_rhythm_map_key_1;
+        rhythm_shifter_key_2 <= init_rhythm_map_key_2;
         score <= 8'b0;
         combo <= 8'b0;
       end else if (clk_8 == 1'b1) begin
         rhythm_shifter <= rhythm_shifter >> 1;
+        rhythm_shifter_key_2 <= rhythm_shifter_key_2 >> 1;
       end
     end
 
     // 00: n/a, 01: perfect, 10: good, 11: miss
-    if (button == 1'b0 && button_last_state == 1'b1) begin
+    if (button_1 == 1'b0 && button_1_last_state == 1'b1) begin
       if (rhythm_shifter[1] == 1'b1) begin
         rhythm_shifter[1] <= 1'b0;
         accuracy <= 2'b10;
@@ -210,13 +218,40 @@ module datapath(clk_8, clk_50m, load, button, is_start, rst, init_rhythm_map, rh
       end
     end
 
-    if (rhythm_shifter[0] == 1'b1) begin
+    if (button_2 == 1'b0 && button_2_last_state == 1'b1) begin
+      if (rhythm_shifter_key_2[1] == 1'b1) begin
+        rhythm_shifter_key_2[1] <= 1'b0;
+        accuracy <= 2'b10;
+        score <= score + 8'd1;
+        combo <= combo + 8'd1;
+      end else if (rhythm_shifter_key_2[2] == 1'b1) begin
+        rhythm_shifter_key_2[2] <= 1'b0;
+        accuracy <= 2'b01;
+        score <= score + 8'd2;
+        combo <= combo + 8'd1;
+      end else if (rhythm_shifter_key_2[3] == 1'b1) begin
+        rhythm_shifter_key_2[3] <= 1'b0;
+        accuracy <= 2'b10;
+        score <= score + 8'd1;
+        combo <= combo + 8'd1;
+      end else begin
+        accuracy <= 2'b00;
+      end
+    end
+
+    if ((rhythm_shifter[0] == 1'b1) || (rhythm_shifter_key_2[0] == 1'b1)) begin
       combo <= 1'b0;
       accuracy <= 2'b11;
     end
 
-    button_last_state <= button;
+    button_1_last_state <= button_1;
+    button_2_last_state <= button_2;
   end
+
+  wire [15:0] shifter_out_color_r;
+  wire [15:0] shifter_out_color_g;
+  assign shifter_out_color_r = rhythm_shifter[15:0];
+  assign shifter_out_color_g = rhythm_shifter_key_2[15:0];
 
   always @(posedge clk_50m) begin
     if (position == 15'b100000000000000) begin
@@ -225,7 +260,7 @@ module datapath(clk_8, clk_50m, load, button, is_start, rst, init_rhythm_map, rh
 
     x_pos[6:0] <= position[13:7];
     y_pos[6:0] <= position[6:0];
-	 
+
     if (x_pos < 7'd8 && y_pos < 7'd8 && !rhythm_shifter[2]) begin
       colour <= 3'b100;
     end else if ((y_pos < 7'd8) && (rhythm_shifter[x_pos / 8 + 7'd2])) begin
